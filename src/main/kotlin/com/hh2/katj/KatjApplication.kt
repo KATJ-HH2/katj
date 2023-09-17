02f58386fe30faf44ec4ecd419340bb0444530f8
JangHyeonJun2 <epfvkdlxj@naver.com>
2023-09-17 12:39:44 +0900

    add: 초기 프로젝트 생성

---
+++ src/main/kotlin/com/hh2/katj/KatjApplication.kt
@@ -0,0 +1,11 @@
+package com.hh2.katj
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KatjApplication
+
+fun main(args: Array<String>) {
+	runApplication<KatjApplication>(*args)
+}

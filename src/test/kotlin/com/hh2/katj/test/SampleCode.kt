3d2682946dd44da6571e3854ac44817a06831397
JangHyeonJun2 <epfvkdlxj@naver.com>
2023-09-24 05:18:57 +0900

    update: build cache 코드 수정, 테스트결과문 등록 코드 추가 등등..

---
+++ src/test/kotlin/com/hh2/katj/test/SampleCode.kt
@@ -0,0 +1,16 @@
+package com.hh2.katj.test
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class SampleCode {
+    @Test
+    fun `일 더하기 일은 이다`() {
+        //given
+        val number1 = 1
+        val number2 = 1
+
+        // when, then
+        assertEquals(2, number1+number2)
+    }
+}

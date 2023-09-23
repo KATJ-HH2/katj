dec428ae27cf0cf22c5d62fee331d5ea03438943
JangHyeonJun2 <epfvkdlxj@naver.com>
2023-09-24 03:50:47 +0900

    update: h2 dependency 삭제, dev-main CI 스크립트 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,8 +26,6 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	runtimeOnly("com.mysql:mysql-connector-j")
-	// https://mvnrepository.com/artifact/com.h2database/h2
-	runtimeOnly ("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 

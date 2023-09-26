c6f704f672883d0fc5a44739f1357c0df50ca203
ktony <k.tony9412@gmail.com>
2023-09-26 15:22:24 +0900

    add: JpaAuditing 기능 추가
    BaseEntity, JpaConfig

---
+++ src/main/kotlin/com/hh2/katj/cmn/model/BaseEntity.kt
@@ -0,0 +1,33 @@
+package com.hh2.katj.cmn.model
+
+import jakarta.persistence.Column
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedBy
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedBy
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+abstract class BaseEntity{
+
+    @CreatedDate
+    @Column(updatable = false)
+    var createdAt: LocalDateTime = LocalDateTime.now()
+        protected set
+
+    @CreatedBy
+    @Column(updatable = false)
+    lateinit var createdBy: String
+
+    @LastModifiedDate
+    var updatedAt: LocalDateTime = LocalDateTime.now()
+        protected set
+
+    @LastModifiedBy
+    lateinit var updatedBy: String
+
+}

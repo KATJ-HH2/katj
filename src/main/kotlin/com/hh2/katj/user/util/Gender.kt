110b06a33f3a309133130f785925db89a8e93f65
ktony <k.tony9412@gmail.com>
2023-09-26 15:12:37 +0900

    add: User 도메인 관련
    User, UserComponent, UserRepository, UserService, UserStatus, Gender

---
+++ src/main/kotlin/com/hh2/katj/user/util/Gender.kt
@@ -0,0 +1,6 @@
+package com.hh2.katj.user.util
+
+enum class Gender(s: String) {
+        MALE("남자"),
+        FEMALE("여자")
+}
---
+++ src/main/kotlin/com/hh2/katj/user/util/UserStatus.kt
@@ -0,0 +1,6 @@
+package com.hh2.katj.user.util
+
+enum class UserStatus(status: String) {
+    ACTIVE("활성"),
+    INACTIVE("비활성")
+}

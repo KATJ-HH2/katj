e56401e40d822d5c4439bad1522228c46894e724
ktony <k.tony9412@gmail.com>
2023-09-27 14:17:59 +0900

    add: 즐겨찾기 전체 조회 추가
    사용자가 등록한 즐겨찾기 전체 조회 추가

--- src/main/kotlin/com/hh2/katj/favorite/component/FavoriteReader.kt
+++ src/main/kotlin/com/hh2/katj/favorite/component/FavoriteReader.kt
@@ -1,12 +1,14 @@
 package com.hh2.katj.favorite.component
 
-import com.hh2.katj.cmn.model.RoadAddress
 import com.hh2.katj.favorite.model.Favorite
+import com.hh2.katj.favorite.model.RequestFavorite
 import com.hh2.katj.favorite.repository.FavoriteRepository
+import com.hh2.katj.user.model.User
 import com.hh2.katj.user.repository.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Component
+import java.lang.IllegalArgumentException
 
 @Component
 class FavoriteReader @Autowired constructor(
@@ -14,26 +16,43 @@ class FavoriteReader @Autowired constructor(
         private val userRepository: UserRepository
 
 ) {
-    fun addFavorite(userId: Long?, roadAddress: RoadAddress, title: String, description: String?): Boolean {
-        val findUser = userRepository.findByIdOrNull(userId) ?: return false
-
-        val favorite: Favorite = Favorite(
-                user = findUser,
-                roadAddress = roadAddress,
-                title = title,
-                description = description
-        )
+    fun addFavorite(userId: Long?, request: RequestFavorite): Boolean {
+        // user 찾기 실패시 오류 반환 또는 false 반환 정책 정해야 할듯
+        val findUser = userValidation(userId)
 
         // 즐겨찾기 타이틀 중복 체크
-        val favoriteTitleDuplicate = favoriteRepository.findByTitle(title).isEmpty
+        val favoriteTitleDuplicate = favoriteRepository.findByTitle(request.title).isEmpty
+
         if (!favoriteTitleDuplicate) {
             // 중복시 오류 반환? false 반환?
             return false
         }
 
+        val favorite: Favorite = Favorite(
+                user = findUser,
+                roadAddress = request.roadAddress,
+                title = request.title,
+                description = request.description
+        )
+
         favoriteRepository.save(favorite)
 
         return true
+    }
+
+    fun findAllFavorite(userId: Long?): MutableList<Favorite> {
+        val findUser = userValidation(userId)
+
+        val findAllFavorite = favoriteRepository.findAll()
+
+        return findAllFavorite
+    }
+
+    /**
+     * 유저 유효성 체크
+     */
+    private fun userValidation(userId: Long?): User {
+        return userRepository.findByIdOrNull(userId) ?: throw IllegalArgumentException("user doesn't exist")
     }
 
 }

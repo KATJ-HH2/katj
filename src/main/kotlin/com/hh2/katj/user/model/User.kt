d8bb9b5b184449789b613f00a33649dff2fda53c
ktony <k.tony9412@gmail.com>
2023-09-28 23:05:31 +0900

    update: User Entity 수정
    equals, hashCode, toString ㄱ현

--- src/main/kotlin/com/hh2/katj/user/model/User.kt
+++ src/main/kotlin/com/hh2/katj/user/model/User.kt
@@ -36,4 +36,42 @@ class User (
 
         var status: UserStatus
 ): BaseEntity(){
+
+        override fun equals(other: Any?): Boolean {
+                if (this === other) return true
+                if (javaClass != other?.javaClass) return false
+
+                other as User
+
+                if (id != other.id) return false
+                if (favorites != other.favorites) return false
+                if (name != other.name) return false
+                if (phoneNumber != other.phoneNumber) return false
+                if (email != other.email) return false
+                if (gender != other.gender) return false
+                if (paymentMethods != other.paymentMethods) return false
+                if (roadAddress != other.roadAddress) return false
+                if (status != other.status) return false
+
+                return true
+        }
+
+        override fun hashCode(): Int {
+                var result = id?.hashCode() ?: 0
+                result = 31 * result + (favorites?.hashCode() ?: 0)
+                result = 31 * result + name.hashCode()
+                result = 31 * result + phoneNumber.hashCode()
+                result = 31 * result + email.hashCode()
+                result = 31 * result + (gender?.hashCode() ?: 0)
+                result = 31 * result + (paymentMethods?.hashCode() ?: 0)
+                result = 31 * result + (roadAddress?.hashCode() ?: 0)
+                result = 31 * result + status.hashCode()
+                return result
+        }
+
+        override fun toString(): String {
+                return "User(id=$id, favorites=$favorites, name='$name', phoneNumber='$phoneNumber', email='$email', gender=$gender, paymentMethods=$paymentMethods, roadAddress=$roadAddress, status=$status)"
+        }
+
+
 }

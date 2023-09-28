07fd8cfe9e2899ff9e8467c98e5df080ab423db0
ktony <k.tony9412@gmail.com>
2023-09-28 23:09:51 +0900

    update: Favorite, RoadAddress
    equals, hashCode, toString 구현

--- src/main/kotlin/com/hh2/katj/cmn/model/RoadAddress.kt
+++ src/main/kotlin/com/hh2/katj/cmn/model/RoadAddress.kt
@@ -17,4 +17,48 @@ class RoadAddress (
         private var x: String,
         private var y: String
 ) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as RoadAddress
+
+        if (addressName != other.addressName) return false
+        if (region1depthName != other.region1depthName) return false
+        if (region2depthName != other.region2depthName) return false
+        if (region3depthName != other.region3depthName) return false
+        if (roadName != other.roadName) return false
+        if (undergroundYn != other.undergroundYn) return false
+        if (mainBuildingNo != other.mainBuildingNo) return false
+        if (subBuildingNo != other.subBuildingNo) return false
+        if (buildingName != other.buildingName) return false
+        if (zoneNo != other.zoneNo) return false
+        if (x != other.x) return false
+        if (y != other.y) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = addressName.hashCode()
+        result = 31 * result + region1depthName.hashCode()
+        result = 31 * result + region2depthName.hashCode()
+        result = 31 * result + region3depthName.hashCode()
+        result = 31 * result + roadName.hashCode()
+        result = 31 * result + undergroundYn.hashCode()
+        result = 31 * result + mainBuildingNo.hashCode()
+        result = 31 * result + subBuildingNo.hashCode()
+        result = 31 * result + buildingName.hashCode()
+        result = 31 * result + zoneNo.hashCode()
+        result = 31 * result + x.hashCode()
+        result = 31 * result + y.hashCode()
+        return result
+    }
+
+    override fun toString(): String {
+        return "RoadAddress(addressName='$addressName', region1depthName='$region1depthName', region2depthName='$region2depthName', region3depthName='$region3depthName', roadName='$roadName', undergroundYn='$undergroundYn', mainBuildingNo='$mainBuildingNo', subBuildingNo='$subBuildingNo', buildingName='$buildingName', zoneNo='$zoneNo', x='$x', y='$y')"
+    }
+
+
 }
--- src/main/kotlin/com/hh2/katj/favorite/model/Favorite.kt
+++ src/main/kotlin/com/hh2/katj/favorite/model/Favorite.kt
@@ -29,4 +29,34 @@ class Favorite(
         var description: String?
 
 ):BaseEntity() {
+
+        override fun equals(other: Any?): Boolean {
+                if (this === other) return true
+                if (javaClass != other?.javaClass) return false
+
+                other as Favorite
+
+                if (id != other.id) return false
+                if (user != other.user) return false
+                if (roadAddress != other.roadAddress) return false
+                if (title != other.title) return false
+                if (description != other.description) return false
+
+                return true
+        }
+
+        override fun hashCode(): Int {
+                var result = id?.hashCode() ?: 0
+                result = 31 * result + user.hashCode()
+                result = 31 * result + roadAddress.hashCode()
+                result = 31 * result + title.hashCode()
+                result = 31 * result + (description?.hashCode() ?: 0)
+                return result
+        }
+
+        override fun toString(): String {
+                return "Favorite(id=$id, user=$user, roadAddress=$roadAddress, title='$title', description=$description)"
+        }
+
+
 }

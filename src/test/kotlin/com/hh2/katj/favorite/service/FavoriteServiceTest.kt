59ea368a3c46b76332165c22f864fdb73f101b09
ktony <k.tony9412@gmail.com>
2023-10-05 17:41:08 +0900

    update: FavoriteServiceTest 케이스 추가, 수정
    테스트 케이스 추가 : 사용자가 등록한 즐겨찾기 중 하나만 선택하여 조회한다
    
    사용자가 즐겨찾기를 전부 조회한다
    : 단순 사이즈 확인 -> 상세 정보 비교 추가

--- src/test/kotlin/com/hh2/katj/favorite/service/FavoriteServiceTest.kt
+++ src/test/kotlin/com/hh2/katj/favorite/service/FavoriteServiceTest.kt
@@ -144,6 +144,88 @@ class FavoriteServiceTest @Autowired constructor(
         }
     }
 
+    @Test
+    fun `사용자가 등록한 즐겨찾기 중 하나만 선택하여 조회한다`() {
+        // given
+        val roadAddressA: RoadAddress = RoadAddress(
+                addressName = "address_name",
+                region1depthName = "r_1",
+                region2depthName = "r_2",
+                region3depthName = "r_3",
+                roadName = "road_name",
+                undergroundYn = "Y",
+                mainBuildingNo = "1",
+                subBuildingNo = "2",
+                buildingName = "bn",
+                zoneNo = "11232",
+                x = "x.123",
+                y = "y.321"
+        )
+
+        val user: User = User(
+                name = "newUser",
+                phoneNumber = "123-456-789",
+                email = "user@gmail.com",
+                gender = Gender.MALE,
+                status = UserStatus.ACTIVE,
+                roadAddress = roadAddressA
+        )
+
+
+        val roadAddressB: RoadAddress = RoadAddress(
+                addressName = "address_name",
+                region1depthName = "r_1",
+                region2depthName = "r_2",
+                region3depthName = "r_3",
+                roadName = "road_name",
+                undergroundYn = "Y",
+                mainBuildingNo = "1",
+                subBuildingNo = "2",
+                buildingName = "bn",
+                zoneNo = "11232",
+                x = "x.123",
+                y = "y.321"
+        )
+
+        val favoriteA: Favorite = Favorite(
+                user = user,
+                roadAddress = roadAddressA,
+                title = "favorite_titleA",
+                description = "favorite_descriptionA"
+        )
+
+        val favoriteB: Favorite = Favorite(
+                user = user,
+                roadAddress = roadAddressB,
+                title = "favorite_titleB",
+                description = "favorite_descriptionB"
+        )
+
+        // when
+        val saveUser = userRepository.save(user)
+
+        val requestAddFavoriteA: RequestAddFavorite = RequestAddFavorite(
+                roadAddress = roadAddressA,
+                favoriteA.title,
+                favoriteA.description
+        )
+        val requestAddFavoriteB: RequestAddFavorite = RequestAddFavorite(
+                roadAddress = roadAddressB,
+                favoriteB.title,
+                favoriteB.description
+        )
+        favoriteService.addFavorite(saveUser.id, requestAddFavoriteA)
+        favoriteService.addFavorite(saveUser.id, requestAddFavoriteB)
+
+        val favorites: MutableList<Favorite> = favoriteService.findAllFavorite(saveUser.id)
+
+        // then
+        val findFavoriteA = favorites.first { it.title == requestAddFavoriteA.title }
+        assertThat(findFavoriteA.roadAddress).isEqualTo(requestAddFavoriteA.roadAddress)
+        assertThat(findFavoriteA.title).isEqualTo(requestAddFavoriteA.title)
+        assertThat(findFavoriteA.description).isEqualTo(requestAddFavoriteA.description)
+
+    }
 
     @Test
     fun `사용자가 즐겨찾기를 전부 조회한다`() {
@@ -222,6 +304,13 @@ class FavoriteServiceTest @Autowired constructor(
 
         // then
         Assertions.assertThat(favorites.size).isEqualTo(2)
+
+        assertThat(favorites).extracting("roadAddress")
+                .containsExactlyInAnyOrder(requestAddFavoriteA.roadAddress, requestAddFavoriteB.roadAddress)
+        assertThat(favorites).extracting("title")
+                .containsExactlyInAnyOrder(requestAddFavoriteA.title, requestAddFavoriteB.title)
+        assertThat(favorites).extracting("description")
+                .containsExactlyInAnyOrder(requestAddFavoriteA.description, requestAddFavoriteB.description)
 
     }
     

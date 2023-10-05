f8aad66782d871f8aee671a69b1d65d4e977ddbd
ktony <k.tony9412@gmail.com>
2023-10-05 14:31:45 +0900

    add: ExceptionUtils 생성
    repository 사용시 발생할 수 있는 예외에 대해 좀 더 깔끔하게 처리 할 수 있도록 템플릿 추가

---
+++ src/main/kotlin/com/hh2/katj/util/exception/ExceptionUtils.kt
@@ -0,0 +1,12 @@
+package com.hh2.katj.util.exception
+
+import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.findByIdOrNull
+
+fun fail(): Nothing {
+    throw IllegalArgumentException()
+}
+
+fun <T, ID> CrudRepository<T, ID>.findByIdOrThrow(id: ID): T {
+    return this.findByIdOrNull(id) ?: fail()
+}

dae346e0826edbec05ff9b53ac705f49d2b99161
ktony <k.tony9412@gmail.com>
2023-10-06 18:21:33 +0900

    update: ExceptionMessage
    내부 데이터베이스 서버 에러 오류 메시지 추가

--- src/main/kotlin/com/hh2/katj/util/exception/ExceptionMessage.kt
+++ src/main/kotlin/com/hh2/katj/util/exception/ExceptionMessage.kt
@@ -4,4 +4,5 @@ enum class ExceptionMessage(message: String, status: Int) {
     ID_DOES_NOT_EXIST("id does not exist", 400),
     DUPLICATED_DATA_ALREADY_EXISTS("duplicated data already exists", 400),
     USER_DOES_NOT_EXIST("user does not exist", 400),
+    INTERNAL_SERVER_ERROR_FROM_DATABASE("internal server error from database", 500)
 }

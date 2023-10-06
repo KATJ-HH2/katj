60507fbf439369c449686ff2f9bbede57281a0d3
ktony <k.tony9412@gmail.com>
2023-10-06 18:57:47 +0900

    update: FavoriteRepository 추가
    delete 관련 query methods 일 경우 쿼리가 각각 나가는걸 보완하기 위해 JPQL로 작성

--- src/main/kotlin/com/hh2/katj/favorite/repository/FavoriteRepository.kt
+++ src/main/kotlin/com/hh2/katj/favorite/repository/FavoriteRepository.kt
@@ -2,10 +2,22 @@ package com.hh2.katj.favorite.repository
 
 import com.hh2.katj.favorite.model.entity.Favorite
 import org.springframework.data.jpa.repository.JpaRepository
-import java.util.*
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface FavoriteRepository : JpaRepository<Favorite, Long> {
     fun findByTitle(title: String): Favorite?
     fun findByUserIdAndId(userId: Long?, favoriteId: Long?): Favorite?
+
+    fun findAllByUserId(userId: Long?): MutableList<Favorite>
+
+    @Modifying
+    @Query("DELETE FROM Favorite f WHERE f.user.id = :userId")
+    fun deleteAllByUserId(@Param("userId") userId: Long?)
+
+    @Modifying
+    @Query("DELETE FROM Favorite f WHERE f.user.id = :userId AND f.id IN :deleteFavoriteIds")
+    fun deleteFavoritesByUserIdAndIdIn(@Param("userId") userId: Long?, @Param("deleteFavoriteIds") deleteFavoriteIds: List<Long?>): Int
 
 }

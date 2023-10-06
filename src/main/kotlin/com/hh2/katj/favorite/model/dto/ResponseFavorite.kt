fcddfaa9c06bbe2658db8e8fb0713bb80d05a087
ktony <k.tony9412@gmail.com>
2023-10-06 21:58:00 +0900

    add: ResponseFavorite 생성
    service layer 의 조회 / 수정 로직 수행시 반환 타입으로 Entity가 아닌 DTO 사용

---
+++ src/main/kotlin/com/hh2/katj/favorite/model/dto/ResponseFavorite.kt
@@ -0,0 +1,32 @@
+package com.hh2.katj.favorite.model.dto
+
+import com.hh2.katj.favorite.model.entity.Favorite
+import com.hh2.katj.user.model.entity.User
+import com.hh2.katj.util.model.RoadAddress
+
+/**
+ * @author : tony
+ * @description : 즐겨찾기 조회 후 결과 반환시 Controller 사용 목적의 DTO
+ * @since : 2023-10-06
+ */
+data class ResponseFavorite(
+        val id: Long?,
+        var roadAddress: RoadAddress,
+        var title: String,
+        var description: String?,
+        var user: User,
+)  {
+
+    companion object {
+        fun of(favorite: Favorite): ResponseFavorite {
+            return ResponseFavorite(
+                    id = favorite.id,
+                    roadAddress = favorite.roadAddress,
+                    title = favorite.title,
+                    description = favorite.description,
+                    user = favorite.user,
+            )
+        }
+    }
+
+}

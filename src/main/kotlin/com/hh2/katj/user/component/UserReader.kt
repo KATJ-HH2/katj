a01c38ca3067cb5051a7ef20fde3c26d6e84eefd
ktony <k.tony9412@gmail.com>
2023-10-06 18:20:34 +0900

    add: UserReader 추가
    1.user 관련 조회 트랜잭션을 관리하는 Component 추가
    2.userValidation 기능을 FavoriteReader에서 분리하여 FavoriteService에서 userReader를 사용하도록 변경

---
+++ src/main/kotlin/com/hh2/katj/user/component/UserReader.kt
@@ -0,0 +1,19 @@
+package com.hh2.katj.user.component
+
+import com.hh2.katj.user.model.entity.User
+import com.hh2.katj.user.repository.UserRepository
+import com.hh2.katj.util.exception.ExceptionMessage
+import com.hh2.katj.util.exception.findByIdOrThrowMessage
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+
+@Component
+class UserReader(
+        private val userRepository: UserRepository
+) {
+
+    @Transactional(readOnly = true)
+    fun userValidation(userId: Long?): User =
+            userRepository.findByIdOrThrowMessage(userId, ExceptionMessage.USER_DOES_NOT_EXIST.name)
+
+}

dbb3f2ef11f07f21f3bf732014accac95d9754bf
ktony <k.tony9412@gmail.com>
2023-10-09 00:31:18 +0900

    update: FavoriteController 수정
    TDD로 구축한 FavoriteService 사용 EndPoint 작성 및 검증 완료

--- src/main/kotlin/com/hh2/katj/favorite/controller/FavoriteController.kt
+++ src/main/kotlin/com/hh2/katj/favorite/controller/FavoriteController.kt
@@ -1,15 +1,13 @@
 package com.hh2.katj.favorite.controller
 
 import com.hh2.katj.favorite.model.dto.RequestAddFavorite
+import com.hh2.katj.favorite.model.dto.RequestUpdateFavorite
 import com.hh2.katj.favorite.model.dto.ResponseFavorite
 import com.hh2.katj.favorite.service.FavoriteService
 import com.hh2.katj.user.service.UserService
 import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RequestMapping("/favorite")
 @RestController
@@ -18,13 +16,74 @@ class FavoriteController (
     private val userService: UserService,
 ){
 
+    /**
+     * 해당 사용자에 즐겨찾기 추가
+     */
     @PostMapping
-    fun addFavorite(@Valid @RequestBody requestAddFavorite: RequestAddFavorite, userId: Long): ResponseEntity<ResponseFavorite> {
+    fun addFavorite(@RequestParam userId: Long, @Valid @RequestBody requestAddFavorite: RequestAddFavorite): ResponseEntity<ResponseFavorite> {
 
         val findUser = userService.findByUserId(userId)
         val responseFavorite = favoriteService.addFavorite(requestAddFavorite.toEntity(findUser))
 
         return ResponseEntity.ok(responseFavorite)
+    }
+
+
+    /**
+     * 해당 사용자의 모든 즐겨찾기 조회
+     */
+    @GetMapping
+    fun findAllFavorite(@RequestParam userId: Long): ResponseEntity<List<ResponseFavorite>>{
+        val responseFavoriteList = favoriteService.findAllFavorite(userId)
+        return ResponseEntity.ok(responseFavoriteList)
+    }
+
+    /**
+     * 해당 사용자의 해당 즐겨찾기 조회
+     */
+    @GetMapping("/{favoriteId}")
+    fun findOneFavorite(@RequestParam userId: Long, @PathVariable("favoriteId") favoriteId: Long): ResponseEntity<ResponseFavorite> {
+        val findOneFavorite = favoriteService.findOneFavorite(userId, favoriteId)
+        return ResponseEntity.ok(findOneFavorite)
+    }
+
+    /**
+     * 해당 사용자의 해당 즐겨찾기 수정
+     */
+    @PutMapping("/{favoriteId}")
+    fun updateFavorite(@RequestParam userId: Long, @PathVariable("favoriteId") favoriteId: Long, @RequestBody requestUpdateFavorite: RequestUpdateFavorite): ResponseEntity<ResponseFavorite> {
+
+        val findUser = userService.findByUserId(userId)
+        val updateFavorite = favoriteService.updateFavorite(favoriteId, requestUpdateFavorite.toEntity(findUser))
+        return ResponseEntity.ok(updateFavorite)
+    }
+
+    /**
+     * 해당 사용자의 해당 즐겨찾기 삭제
+     */
+    @DeleteMapping("/{favoriteId}")
+    fun deleteOneFavorite(@RequestParam userId: Long, @PathVariable("favoriteId") favoriteId: Long): ResponseEntity<Boolean> {
+        val deleteOneFavorite = favoriteService.deleteOneFavorite(userId, favoriteId)
+        return ResponseEntity.ok(deleteOneFavorite)
+    }
+
+    /**
+     * 해당 사용자의 요청온 모든 즐겨찾기 삭제
+     */
+    @DeleteMapping("/delete-multi")
+    fun deleteMultiFavorite(@RequestParam userId: Long, @RequestBody deleteFavoriteIds: List<Long>): ResponseEntity<Boolean> {
+        val deleteOneFavorite = favoriteService.deleteMultiFavorite(userId, deleteFavoriteIds)
+        return ResponseEntity.ok(deleteOneFavorite)
+    }
+
+
+    /**
+     * 해당 사용자의 모든 즐겨찾기 삭제
+     */
+    @DeleteMapping("/delete-all")
+    fun deleteAllFavorite(@RequestParam userId: Long): ResponseEntity<Boolean> {
+        val deleteOneFavorite = favoriteService.deleteAllFavorite(userId)
+        return ResponseEntity.ok(deleteOneFavorite)
     }
 
 }

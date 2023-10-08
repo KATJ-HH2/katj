62a90c29aff7d69ae00b4ba348493104a625149a
ktony <k.tony9412@gmail.com>
2023-10-08 15:50:22 +0900

    update: UserManager 수정
    1.즐겨찾기 생성시 유저를 추가하는 메서드 생성
    2.userId로 User를 찾는 메서드 생성

--- src/main/kotlin/com/hh2/katj/user/component/UserManager.kt
+++ src/main/kotlin/com/hh2/katj/user/component/UserManager.kt
@@ -1,8 +1,10 @@
 package com.hh2.katj.user.component
 
+import com.hh2.katj.favorite.model.entity.Favorite
 import com.hh2.katj.user.model.entity.User
 import com.hh2.katj.user.repository.UserRepository
-import com.hh2.katj.util.exception.ExceptionMessage
+import com.hh2.katj.util.exception.ExceptionMessage.ID_DOES_NOT_EXIST
+import com.hh2.katj.util.exception.ExceptionMessage.USER_DOES_NOT_EXIST
 import com.hh2.katj.util.exception.findByIdOrThrowMessage
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
@@ -14,6 +16,17 @@ class UserManager(
 
     @Transactional(readOnly = true)
     fun userValidation(userId: Long): User =
-            userRepository.findByIdOrThrowMessage(userId, ExceptionMessage.USER_DOES_NOT_EXIST.name)
+            userRepository.findByIdOrThrowMessage(userId, USER_DOES_NOT_EXIST.name)
+
+    @Transactional
+    fun addFavoriteToUser(user: User, favorite: Favorite) {
+        favorite.addFavoriteTo(user)
+    }
+
+    @Transactional(readOnly = true)
+    fun findByUserId(userId: Long): User {
+        val findUser = userRepository.findByIdOrThrowMessage(userId, ID_DOES_NOT_EXIST.name)
+        return findUser
+    }
 
 }

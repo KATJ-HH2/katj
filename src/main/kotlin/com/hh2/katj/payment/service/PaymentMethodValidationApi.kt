eb21c94cf9a5f888a8ec6ebe3887de71d602fc64
ktony <k.tony9412@gmail.com>
2023-10-09 21:09:05 +0900

    add: 계좌 / 카드 외부 인증 API
    외부 API가 연동되진 않았지만 추가 예정
    무조건 인증된것으로 하고 true 반환

---
+++ src/main/kotlin/com/hh2/katj/payment/service/PaymentMethodValidationApi.kt
@@ -0,0 +1,16 @@
+package com.hh2.katj.payment.service
+
+import com.hh2.katj.payment.model.entity.PaymentMethod
+import org.springframework.stereotype.Component
+
+@Component
+class PaymentMethodValidationApi {
+    fun bankAccountValidation(paymentMethod: PaymentMethod): Boolean{
+        /**
+         * 외부 API 사용으로 요청 계좌 정보 확인 로직 작성
+         */
+        paymentMethod.changeValidToTrue()
+        return true
+    }
+
+}

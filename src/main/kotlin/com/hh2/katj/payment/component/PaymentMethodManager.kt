7bfb6a86821a9bc5edd76f8f100317df8f7f99f5
ktony <k.tony9412@gmail.com>
2023-10-09 21:13:09 +0900

    add: 결제수단 Component계층 생성
    PaymentMethodManager
    결제수단 관련 생성, 삭제 기능
    
    PaymentMethodReader
    결제수단 관련 조회 기능

---
+++ src/main/kotlin/com/hh2/katj/payment/component/PaymentMethodManager.kt
@@ -0,0 +1,48 @@
+package com.hh2.katj.payment.component
+
+import com.hh2.katj.payment.model.entity.PaymentMethod
+import com.hh2.katj.payment.repository.PaymentMethodRepository
+import com.hh2.katj.util.exception.ExceptionMessage
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+
+@Component
+class PaymentMethodManager (
+    private val paymentMethodRepository: PaymentMethodRepository,
+    private val paymentMethodReader: PaymentMethodReader,
+){
+
+    @Transactional
+    fun addBankAccount(request: PaymentMethod): PaymentMethod {
+        val saveBackAccount = paymentMethodRepository.save(request)
+        return saveBackAccount
+    }
+
+    @Transactional
+    fun changeDefaultToFalse(paymentMethod: PaymentMethod) {
+        paymentMethod.changeDefaultToFalse()
+        paymentMethodRepository.save(paymentMethod)
+    }
+
+    @Transactional
+    fun deleteOnePaymentMethod(userId: Long, paymentMethodId: Long): Boolean {
+        val deletePaymentMethod = paymentMethodReader.paymentMethodExistValidation(userId, paymentMethodId)
+
+        try {
+            paymentMethodRepository.delete(deletePaymentMethod)
+        } catch (e: Exception) {
+            throw Exception(ExceptionMessage.INTERNAL_SERVER_ERROR_FROM_DATABASE.name)
+        }
+        return true
+    }
+
+    @Transactional
+    fun deleteAllFavorite(userId: Long): Boolean {
+        try {
+            paymentMethodRepository.deleteAllByUserId(userId)
+        } catch (e: Exception) {
+            throw Exception(ExceptionMessage.INTERNAL_SERVER_ERROR_FROM_DATABASE.name)
+        }
+        return true
+    }
+}

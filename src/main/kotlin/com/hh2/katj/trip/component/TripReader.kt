5ddce5262f569666788c92bd1bc49f8fd33bc27d
ktony <k.tony9412@gmail.com>
2023-10-12 01:49:30 +0900

    update: tripReader 수정
    userId와 tripId로 조회 하는 메서드 추가

--- src/main/kotlin/com/hh2/katj/trip/component/TripReader.kt
+++ src/main/kotlin/com/hh2/katj/trip/component/TripReader.kt
@@ -2,7 +2,9 @@ package com.hh2.katj.trip.component
 
 import com.hh2.katj.trip.model.Trip
 import com.hh2.katj.trip.repository.TripRepository
+import com.hh2.katj.util.exception.ExceptionMessage
 import com.hh2.katj.util.exception.ExceptionMessage.ID_DOES_NOT_EXIST
+import com.hh2.katj.util.exception.failWithMessage
 import com.hh2.katj.util.exception.findByIdOrThrowMessage
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
@@ -12,8 +14,14 @@ class TripReader (
     private val tripRepository: TripRepository,
 ){
     @Transactional(readOnly = true)
-    fun findByTripId(tripId: Long): Trip {
+    fun findById(tripId: Long): Trip {
         val findTrip = tripRepository.findByIdOrThrowMessage(tripId, ID_DOES_NOT_EXIST.name)
         return findTrip
+    }
+
+    @Transactional(readOnly = true)
+    fun findByIdAndUserId(tripId: Long, userId: Long): Trip {
+        return tripRepository.findByIdAndUserId(tripId, userId) ?:
+                failWithMessage(ID_DOES_NOT_EXIST.name)
     }
 }

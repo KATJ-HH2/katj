cf8a74c0d5f9d5111a3702a27bd3996fba8174bf
ktony <k.tony9412@gmail.com>
2023-10-14 19:36:26 +0900

    update: 사용자가 택시 호출시 사용하는 Service & Test 추가
    RequestTrip 사용자가 택시 호출시 만들어질 Trip 테이블에 사용할 DTO

---
+++ src/main/kotlin/com/hh2/katj/trip/model/request/RequestTrip.kt
@@ -0,0 +1,41 @@
+package com.hh2.katj.trip.model.request
+
+import com.hh2.katj.taxidriver.model.TaxiDriver
+import com.hh2.katj.trip.model.DepartureRoadAddress
+import com.hh2.katj.trip.model.DestinationRoadAddress
+import com.hh2.katj.trip.model.Trip
+import com.hh2.katj.trip.model.TripStatus
+import com.hh2.katj.user.model.entity.User
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+data class RequestTrip (
+    val user: User,
+    val taxiDriver: TaxiDriver?,
+    val fare: Int,
+    val departure: DepartureRoadAddress,
+    val destination: DestinationRoadAddress,
+    val driveStartDate: LocalDate,
+    val driveStartAt: LocalDateTime,
+    val driveEndDate: LocalDate? = null,
+    val driveEndAt: LocalDateTime? = null,
+    val spentTime: Int,
+    val tripStatus: TripStatus
+) {
+    fun toEntity(): Trip {
+        return Trip(
+            user = this.user,
+            taxiDriver = this.taxiDriver,
+            fare = this.fare,
+            departure = this.departure,
+            destination = this.destination,
+            driveStartDate = this.driveStartDate,
+            driveStartAt = this.driveStartAt,
+            driveEndDate = this.driveEndDate,
+            driveEndAt = this.driveEndAt,
+            spentTime = this.spentTime,
+            tripStatus = this.tripStatus,
+        )
+    }
+
+}

6a1c22bfabb69fdfcc7bd99c36d06b2ff7914184
JangHyeonJun2 <epfvkdlxj@naver.com>
2023-10-14 13:56:09 +0900

    update: 택시 생성 request validation

--- src/main/kotlin/com/hh2/katj/taxi/model/dto/RequestAddTaxi.kt
+++ src/main/kotlin/com/hh2/katj/taxi/model/dto/RequestAddTaxi.kt
@@ -3,16 +3,34 @@ package com.hh2.katj.taxi.model.dto
 import com.hh2.katj.taxi.model.ChargeType
 import com.hh2.katj.taxi.model.FuelType
 import com.hh2.katj.taxi.model.Taxi
+import jakarta.validation.Valid
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
 import java.time.LocalDate
 
 data class RequestAddTaxi(
+    @NotBlank(message = "차량번호는 필수 입니다.")
     val carNo: String,
+
+    @NotBlank(message = "차대번호는 필수 입니다.")
     val vin: String,
+
+    @NotNull(message = "")
     val kind: ChargeType,
+
+    @NotNull(message = "제조일자는 필수 입니다.")
     val manufactureDate: LocalDate,
+
+    @NotNull(message = "연료 타입을 선택해주세요.")
     val fuel: FuelType,
+
+    @NotBlank(message = "색상은 필수입니다.")
     val color: String,
+
+    @NotNull(message = "보험여부 값은 필수입니다.")
     val insureYn: Boolean,
+
+    @NotNull(message = "사고여부 값은 필수입니다.")
     val accidentYn: Boolean
 )
 

e27fbabd032dbec38e724dca222daa20fe14f954
JangHyeonJun2 <epfvkdlxj@naver.com>
2023-10-17 02:14:04 +0900

    delete: 불필요한 import

--- src/main/kotlin/com/hh2/katj/taxi/controller/TaxiController.kt
+++ src/main/kotlin/com/hh2/katj/taxi/controller/TaxiController.kt
@@ -7,7 +7,6 @@ import com.hh2.katj.taxi.service.TaxiService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.net.URI
 

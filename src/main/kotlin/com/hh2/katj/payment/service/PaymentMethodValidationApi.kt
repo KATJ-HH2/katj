e56dfb776e087acf6bf2d232de4ae4287796a359
ktony <k.tony9412@gmail.com>
2023-10-24 20:40:51 +0900

    update: 결제 수단 등록 외부 API 사용 수정
    profile을 local 사용시에만 true 반환

--- src/main/kotlin/com/hh2/katj/payment/service/PaymentMethodValidationApi.kt
+++ src/main/kotlin/com/hh2/katj/payment/service/PaymentMethodValidationApi.kt
@@ -1,16 +1,21 @@
 package com.hh2.katj.payment.service
 
 import com.hh2.katj.payment.model.entity.PaymentMethod
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 @Component
 class PaymentMethodValidationApi {
+
+    @Value("\${spring.profiles.active}")
+    private val activeProfile: String? = null
+
     fun bankAccountValidation(paymentMethod: PaymentMethod): Boolean{
         /**
          * 외부 API 사용으로 요청 계좌 정보 유효성 확인 로직 작성
          */
         paymentMethod.changeValidToTrue()
-        return true
+        return activeProfile.isLocal()
     }
 
     fun cardValidation(paymentMethod: PaymentMethod): Boolean {
@@ -18,7 +23,11 @@ class PaymentMethodValidationApi {
          * 외부 API 사용으로 요청 카드 정보 유효성 확인 로직 작성
          */
         paymentMethod.changeValidToTrue()
-        return true
+        return activeProfile.isLocal()
     }
 
+}
+
+private fun String?.isLocal(): Boolean {
+    return this.equals("local")
 }

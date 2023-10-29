3cad4646b01f3e1832bce6c4e0545ee38ca0f3d3
JangHyeonJun2 <epfvkdlxj@naver.com>
2023-10-29 17:20:43 +0900

    update: 택시 저장 테스트 코드

---
+++ src/test/kotlin/com/hh2/katj/taxidriver/service/TaxiDriverServiceTest.kt
@@ -0,0 +1,36 @@
+package com.hh2.katj.taxidriver.service
+
+import com.hh2.katj.taxi.fixtures.TaxiTestFixtures
+import com.hh2.katj.taxi.model.ChargeType
+import com.hh2.katj.taxi.model.FuelType
+import com.hh2.katj.taxi.repository.TaxiRepository
+import com.hh2.katj.util.annotation.KATJTestContainerE2E
+import com.hh2.katj.util.model.BaseTestEntity
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.time.LocalDate
+
+@KATJTestContainerE2E
+class TaxiDriverServiceTest(
+    private val taxiRepository: TaxiRepository
+): BaseTestEntity() {
+    @BeforeEach
+    fun setUp() {
+        taxiRepository.deleteAllInBatch()
+    }
+
+    @Test
+    fun `택시_저장`(){
+        val taxi: String = TaxiTestFixtures.택시_생성(
+            "62거4811",
+            "KMHDL41BP8A000001",
+            ChargeType.NORMAL,
+            LocalDate.now(),
+            FuelType.LPG,
+            "YELLOW",
+            true,
+            false
+        )
+    }
+
+}
